a35a9f23ecf265eb88cd4eea68cc8f4e8cecade7
Richard Le <richardle1997@gmail.com>
2019-03-08 12:40:43 -0800

    Can submit grid positions

--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
+
+//WHen the create a garden is clicked, fade out and display a garden creator.
 $('#createGardenButton').click(function(){
 	$('#createGardenButton').fadeOut("slow",function(){
-		// var testArray = [
-		// 	[1, 2],
-		// 	[3, 4],
-		// 	[5, 6]
-		// ];
-		// var description = "A test array";
-		// firebase.database().ref("users/" + user.uid).update({
-		// 	testArray:testArray, 
-		// });
 		$("#contentRow").css({"visibility": "visible", "display": "flex"});
+		buildCreateAGarden();
 	});
 });
 
 
-$('.gardenBtn').click(function(){
-	$(this).toggleClass("selected");
+//Creates a 5 x 5 Garden Grid
+//TODO: Make this accept arbitrary sized array.
+function buildCreateAGarden(){
+	for (var x = 0; x < 5; x++){
+		var newRow = "<div id=" + "row" + (x + 1) + " class=\"btn-group\">";
+		$("#btn-container").append(newRow);
+		var appendedRow = $("#row" + (x + 1));
+		for (var y = 0; y < 5; y++){
+			var newButton = "<button class=\"gardenBtn\" data-pos=\"[" + y + "," + x +"]\">1</button>"
+			$("#row" + (x + 1)).append(newButton);
+		}
+	}
+
+	//When a garden Button is clicked, add the class selected to that button.
+	$('.gardenBtn').click(function(){
+		$(this).toggleClass("selected");
+	});
+}
+
+
+$('#doneBtn').click(function(){
+	buildGrid();
 });
+
+
+function buildGrid(){
+	var gardenArr = [];
+	$(".selected").each(function(){
+		gardenArr.push($(this).data("pos"));
+	});
+
+	var description = "A test array";
+		firebase.database().ref("users/" + "testUser").update({
+		testArray:gardenArr
+	});
+}
